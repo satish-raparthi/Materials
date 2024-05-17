0d4f5d7923926e3e0166aa2db066ac406b8b52cf
Sathiraju <84367633+satish-raparthi@users.noreply.github.com>
2024-05-17 10:13:09 +0530

    Delete constructors directory

--- constructors/TestAccount.java
+++
@@ -1,8 +0,0 @@
-class TestAccount{
-		public static void main(String[] args){
-				Account a=new Account(101,"Pennant",40000);
-				a.deposit(34000);
-				a.withdraw(23000);
-				a.withdraw(60000);
-		}
-}
